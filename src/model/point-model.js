a03029c8e1edf9a75a081bd364110adc013a0a41
Suninall <sanina-nsk@mail.ru>
2025-02-15 16:39:50 +0700

    добавляет приватные поля

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -1,10 +1,13 @@
 export default class PointModel {
+  #points = null;
+  #service = null;
+
   constructor(service) {
-    this.service = service;
-    this.points = this.service.getPoints();
+    this.#service = service;
+    this.#points = this.#service.getPoints();
   }
 
-  get(){
-    return this.points;
+  get point(){
+    return this.#points;
   }
 }
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -5,35 +5,41 @@ import TripPointView from '../view/trip-point-view.js';
 import { render } from '../framework/render.js';
 
 export default class BoardPresenter {
-  sortComponent = new TripSortView();
-  eventListComponent = new TripEventListView();
+  #container = null;
+  #destinationModel = null;
+  #offersModel = null;
+  #pointModel = null;
 
+  #sortComponent = new TripSortView();
+  #eventListComponent = new TripEventListView();
+
+  #points = [];
   constructor({container,offersModel,destinationModel,pointModel}) {
-    this.container = container;
-    this.destinationModel = destinationModel;
-    this.offersModel = offersModel;
-    this.pointModel = pointModel;
+    this.#container = container;
+    this.#destinationModel = destinationModel;
+    this.#offersModel = offersModel;
+    this.#pointModel = pointModel;
 
-    this.points = [...pointModel.get()];
   }
 
   init() {
-    render(this.sortComponent, this.container);
-    render(this.eventListComponent,this.container);
+    this.#points = [...this.#pointModel.point];
+    render(this.#sortComponent, this.#container);
+    render(this.#eventListComponent,this.#container);
 
     render (new TripPointEditView({
-      point: this.points[0],
+      point: this.#points[0],
     }),
-    this.eventListComponent.element);
+    this.#eventListComponent.element);
 
-    this.points.forEach((point) => {
+    this.#points.forEach((point) => {
       render(
         new TripPointView({
           point:point,
-          pointDestination:this.destinationModel.getByID(point.cityInformation.id),
-          pointOffers:this.offersModel.getByType(point.type) || []}
+          pointDestination:this.#destinationModel.getByID(point.cityInformation.id),
+          pointOffers:this.#offersModel.getByType(point.type) || []}
         ),
-        this.eventListComponent.element
+        this.#eventListComponent.element
       );
     });
   }
